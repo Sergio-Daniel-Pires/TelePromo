a533e41fe7c4a5763508676b50360dde5efde216
Sergio Daniel Pires <sergiodanpires@gmail.com>
2023-08-16 20:04:27 -0300

    Bot Pichau pronto

--- telepromo/bots/Pichau.py
+++ telepromo/bots/Pichau.py
@@ -10,10 +10,8 @@ class Pichau(Bot):
 
     async def scroll_to_bottom(self, page) -> None:
         for i in range(8):
-            await asyncio.wait([
-                page.evaluate(f"window.scrollTo(0, document.body.scrollHeight * .{i//2 + 5});"),
-                asyncio.sleep(0.3)
-            ])
+            await page.evaluate(f"window.scrollTo(0, document.body.scrollHeight * .{i//2 + 5});"),
+            await asyncio.sleep(0.3)
 
     async def get_prices(self, **kwargs):
         page = self.page
@@ -34,41 +32,44 @@ class Pichau(Bot):
         
         await self.scroll_to_bottom(page)
 
-        async with page.expect_console_message() as msg_info:
-            results = await page.evaluate(
-            """
-            const resultados = [];
-            try {
-                const section_2 = document.querySelectorAll("section")[1];
-                const produtos = Array.from(section_2.querySelectorAll('a[data-cy="list-product"]'));
-                try {
-                    produtos.forEach((produto) => {
-                        const name = produto.querySelector('h2').innerText;
-                        const details = name;
-                        const price = produto.querySelector('.MuiCardContent-root > div :nth-child(3)').innerText;
-                        const oldPrice = produto.querySelector('.MuiCardContent-root > div :nth-child(1) > div > div > s').innerText;
-                        const url = produto.href;
-                        const img = produto.querySelector('img').src;
-                        resultados.push({name, details, price, oldPrice, url, img});
-                    })
-                } catch (e) {console.log("Erro for: ", e)}
-                console.log("Sucess")
-            } catch (e) {
-                console.log("Erro: ", e);
-            }
-            resultados
-            """
+        second_section = (await page.query_selector_all("section"))[1]
+        for product_selector in (await second_section.query_selector_all('a[data-cy="list-product"]')):
+            name = await (
+                await product_selector.query_selector('h2')
+            ).inner_text()
+            
+            price = await (
+                await product_selector.query_selector('.MuiCardContent-root > div :nth-child(3)')
+            ).inner_text()
+            price = float((price[3:].replace('.', '')).replace(',', '.'))
+            
+            old_price = await (
+                await product_selector.query_selector('.MuiCardContent-root > div :nth-child(1) > div > div > s')
+            ).inner_text()
+            old_price = float((old_price[3:].replace('.', '')).replace(',', '.'))
+            
+            url = await product_selector.get_attribute("href")
+
+            img_selector = await product_selector.query_selector('img')
+
+            if img_selector:
+                img = await (img_selector).get_attribute("src")
+            else:
+                img = None
+
+            all_results.append(
+                {
+                    "name": name, "details": name, "price": price, "oldPrice": old_price,
+                    "url": url, "img": img
+                }
             )
-            print(await msg_info.value)
 
-            all_results += results
-
-        return results
+        return all_results
 
 if __name__ == "__main__":
     bot = Pichau()
     import asyncio
     results = asyncio.run(
-        bot.run(headless=False, link="https://www.pichau.com.br")
+        bot.run(headless=True, link="https://www.pichau.com.br")
     )
     print(results)
