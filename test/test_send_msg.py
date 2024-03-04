6830084b3849e8666d3206f63a31c7292525a6c5
Sergio Pires <s248476@dac.unicamp.br>
2024-03-04 08:38:34 -0300

    Fix repeated sends

--- test/test_send_msg.py
+++ test/test_send_msg.py
@@ -68,7 +68,7 @@ class TestDontSend:
         ]
     )
     @pytest.mark.asyncio
-    async def test_overpriced(self, offer, user_wishes, redis_client, request):
+    async def test_overpriced (self, offer, user_wishes, redis_client, request):
         # Prepare DB for requests
         db = Database(None, mongo_client=mongomock.MongoClient)
 
@@ -102,7 +102,7 @@ class TestDontSend:
         ]
     )
     @pytest.mark.asyncio
-    async def test_blacklisted(self, offer, user_wishes, redis_client: FakeRedis, request):
+    async def test_blacklisted (self, offer, user_wishes, redis_client: FakeRedis, request):
         # Prepare DB for requests
         db = Database(None, mongo_client=mongomock.MongoClient)
 
@@ -124,3 +124,42 @@ class TestDontSend:
 
         last_sent = redis_client.lpop("msgs_to_send")
         assert last_sent is None, "Blacklisted has sent"
+
+    @pytest.mark.parametrize(["offer", "user_wishes"],
+        [
+            (
+                'iphone_offer',
+                [
+                    { "tags": [ "iphone" ], "user": "1", "max_price": 1000 },
+                ]
+            )
+        ]
+    )
+    @pytest.mark.asyncio
+    async def test_repeated (self, offer, user_wishes, redis_client: FakeRedis, request):
+        # Prepare DB for requests
+        db = Database(None, mongo_client=mongomock.MongoClient)
+
+        vectorizers = Vectorizers()
+
+        monitor = Monitoring(
+            retry=3,
+            database=db,
+            vectorizer=vectorizers,
+            redis_client=redis_client,
+            metrics_collector=MagicMock()
+        )
+        monitor.tested_brands = { "Aliexpress", "Magalu" }
+
+        for user_wish in user_wishes:
+            monitor.database.new_wish(**user_wish)
+
+        await monitor.send_offer_to_user(request.getfixturevalue(offer))
+
+        first_send = redis_client.lpop("msgs_to_send")
+        assert first_send is not None, "First need to send, because are not repeated"
+
+        await monitor.send_offer_to_user(request.getfixturevalue(offer))
+
+        second_send = redis_client.lpop("msgs_to_send")
+        assert second_send is None, "Second need to be blocked, because are repeated"
